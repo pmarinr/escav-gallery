c2e393dcebd1f80cf59d8a86e90bfe5b44dee181
Pedro Marín Ramos <pmarinr@gmail.com>
2022-07-14 10:34:06 +0200

    MongoDB connection

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 const port = process.env.PORT || 3000;
+const uri = process.env.MONGODB_URI;
 //create an express app
 const express = require('express');
+const mongoose = require('mongoose');
+const http = require('http');
 const app = express();
 
 // use the express-static middleware
@@ -15,3 +18,14 @@ app.get("/", function (req, res) {
 // start the server listening for requests
 app.listen(port, 
 	() => console.log("Server is running..."));
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const database = mongoose.connection
+
+database.on('error', (error) => {
+  console.log(error)
+})
+
+database.once('connected', () => {
+  console.log('Database Connected');
+})
